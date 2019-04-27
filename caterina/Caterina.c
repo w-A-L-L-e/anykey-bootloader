68bf7e55ffbb9cffdd43ec51092329cbf263662e
Walter Schreppers <schreppers@gmail.com>
2019-04-27 04:39:13 +0200

    make hash length dynamic and fix bug in progress.

--- caterina/Caterina.c
+++ caterina/Caterina.c
@@ -524,14 +524,15 @@ void CDC_Task(void)
   else if (Command == 'K' )
   {
     //block write here until a shared secret is matched, later we will most likely change this into a handshake
-    uint32_t unlock_hash[32]={
+    uint32_t unlock_hash[]={
       0x31, 0x44, 0x41, 0x31, 0x8a, 0x3f, 0x3b, 0xd9,
       0x68, 0x14, 0xa1, 0xd4, 0x2a, 0xbf, 0x4b, 0xab,
       0x41, 0xa4, 0xef, 0xc9, 0xca, 0xff, 0x6b, 0x3d,
       0x33, 0xf4, 0x4e, 0xa3, 0x2a, 0xaf, 0x7b, 0xc1,
     };
+    int unlock_hash_length = sizeof(unlock_hash)/sizeof(unlock_hash[0]);
     uint32_t c, ui=0;
-    while( ui++ < 256 ){
+    while( ui++ < unlock_hash_length ){ 
       c=FetchNextCommandByte();
       if( c!=unlock_hash[ui] ) {
         WriteNextResponseByte(0x01); //signal fail to avrdude
